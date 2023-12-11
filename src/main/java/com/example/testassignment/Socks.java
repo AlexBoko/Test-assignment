0f98c1e2cdac370873cc6ac76dc245a20f147d23
AlexBoko <durgen2014@gmail.com>
2023-12-11 14:56:55 +0300

    Test-assignment-for-the-start-of-employment-No.-1

---
+++ src/main/java/com/example/testassignment/Socks.java
@@ -0,0 +1,68 @@
+package com.example.testassignment;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Socks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String color;
+    private int cottonPart;
+    private int quantity;
+
+    public Socks() {
+    }
+
+    public Socks(String color, int cottonPart, int quantity) {
+        this.color = color;
+        this.cottonPart = cottonPart;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getCottonPart() {
+        return cottonPart;
+    }
+
+    public void setCottonPart(int cottonPart) {
+        this.cottonPart = cottonPart;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Socks{" +
+                "id=" + id +
+                ", color='" + color + '\'' +
+                ", cottonPart=" + cottonPart +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
